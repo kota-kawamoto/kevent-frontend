8454cad5c8202d3f37c802a77d42af1ccae6166e
kawamoto <krt34kk0@gmail.com>
2025-04-26 22:57:57 +0900

    #新規登録の修正

--- src/app/users/lib/getGroups.ts
+++ src/app/users/lib/getGroups.ts
@@ -1,20 +1,10 @@
+import { get } from '@/lib/api'
+
 interface Group {
   id: string
   group_name: string
 }
 
 export async function getGroups(): Promise<Group[]> {
-  const response = await fetch(`${process.env.API_URL}/api/groups`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  })
-
-  if (!response.ok) {
-    throw new Error(`HTTP error status: ${response.status}`)
-  }
-
-  return response.json()
+  return await get('/api/groups')
 }
